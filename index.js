7857e828bc948e68b3b022c45c7f0866498680c0
sirtuck32 <t.long32@hotmail.com>
2015-08-04 14:04:55 -0500

    First push with all the files.

---
+++ index.js
@@ -0,0 +1,90 @@
+Parse.initialize("2ARxWfBXA4aeGt2RHR7n9yqWVVNfq7udIsxG1eCP", "cDIIHTqz2g2r2AStQ6JBH0Bx1Bt87NixajzTTB3p");
+
+function parseSetup() {
+	var TestObject = Parse.Object.extend("TestObject");
+	var testObject = new TestObject();
+	testObject.save({foo: "bar"}).then(function(object) {
+	  alert("yay! it worked");
+	});
+}
+
+function newDiscussion() {
+	var DiscussionObject = Parse.Object.extend("DiscussionObject");
+	var discussionObject = new DiscussionObject();
+
+	discussionObject.set("Title", $("#title").val());
+	discussionObject.set("Desc", $("#desc").val());
+	discussionObject.set("Comment", $("#comment").val());
+	
+	discussionObject.save(null, {
+		success: function(result) {
+		
+			alert("yay! it worked");
+			window.location = "computerScience.html";
+		},
+		error: function(object, error) {
+			alert("failure to create new discussion");
+		}
+	});
+}
+
+function loadDiscussions() {		
+	var container = document.createElement("div");
+	container.className = "container-fluid";
+	
+	var discussions = document.createElement("div");
+	discussions.className = "jumbotron";
+	
+	
+	var DiscussionObject = Parse.Object.extend("DiscussionObject");
+	var query = new Parse.Query(DiscussionObject);
+	
+	query.find({
+		success: function(results) {
+			for(var i = 0; i < results.length; i++) {				
+				var outer = document.createElement("div");
+				outer.className = "col-md-4";
+	
+				var inner = document.createElement("div");
+				inner.className = "thumbnail";
+				inner.style.height = "285px";
+				
+				var caption = document.createElement("div");
+				
+				var title = document.createElement("h3");
+				title.innerHTML = results[i].get("Title");
+				caption.appendChild(title);
+				
+				var desc = document.createElement("div");
+				desc.className = "descBox";
+				desc.innerHTML = results[i].get("Desc");
+				caption.appendChild(desc);
+				
+				caption.appendChild(document.createElement("hr"));
+				
+				var button = document.createElement("a");
+				button.className = "btn btn-primary";
+				button.href = "#";
+				button.innerHTML = "View this discussion";
+				caption.appendChild(button);
+				
+				inner.appendChild(caption);
+				outer.appendChild(inner);
+				discussions.appendChild(outer);
+			}
+			
+			discussions.style.height = (results.length/3 * 375) + "px";
+		},
+		error: function(object, error) {
+			console.log("Error " + error + " " + object);
+		}
+	});
+	
+	var spacing = document.createElement("div");
+	spacing.className = "clearfix visible-lg";
+	discussions.appendChild(spacing);
+	
+	container.appendChild(discussions);
+	
+	document.getElementsByTagName('body')[0].appendChild(container);
+}
